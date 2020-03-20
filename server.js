6cbec44e53764260096200407ac117a5583d4913
shinmm <smitsuno@terpmail.umd.edu>
2020-03-20 02:13:19 -0400

    node/express refactor

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const port = process.env.PORT || 3000;
 // Set public folder as root
 app.use(express.static('public'));
 
-// Allow front-end access to node_modules folder
-app.use('/scripts', express.static(`${__dirname}/node_modules/`));
-
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
-  console.log('listening on %d', port);
+  console.log(`Listening on port : ${port}`);
 });
