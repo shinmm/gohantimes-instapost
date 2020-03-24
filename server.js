db163f08c3d06a5917b01b820a3f7f96400c2a03
shinmm <smitsuno@terpmail.umd.edu>
2020-03-23 23:52:59 -0400

    add option to enter term into internal food dictionary

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config(); // read .env files
 const {Translate} = require('@google-cloud/translate').v2;// Imports the Google Cloud client library
 const express = require('express');
+const fs = require("fs"); // File IO
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,8 +14,8 @@ app.listen(port, () => {
   console.log(`Listening on port : ${port}`);
 });
 
-app.get("/", function(req, res) {
-  res.send('index.html');
+app.get("/home", function(req, res) {
+  res.sendFile('/public/index.html', { root: __dirname });
 });
 
 //:target_lang : Language code, like 'en' for English, 'jpn' for Japanese
@@ -39,4 +40,22 @@ app.get("/api/translate/:target_lang/:text", function(req, res) {
     });
   }
   translateText(text, target_lang);
+});
+
+//JSON for food dictionary terms for translation
+app.get("/api/food-dictionary", function(req, res) {
+  // Read food_dictionary.json file
+  fs.readFile("food_dictionary.json", function(err, data) {
+      // Check for errors
+      if (err) throw err;
+      // Converting to JSON
+      const food_dict = JSON.parse(data);
+      console.log(food_dict); // Print users
+      res.json(food_dict)
+  });
+});
+
+//Way to add to
+app.get("/add-food", function(req, res) {
+  res.sendFile('/public/html/add_food.html', { root: __dirname });
 });
